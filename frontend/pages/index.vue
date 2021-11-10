dda114a26b604473190d9b91cc6fe0998e2edb4f
haru-24 <h24wler@gmail.com>
2021-11-10 15:33:44 +0900

    add:フロントエンド追加

---
+++ frontend/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    {{ name }}
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    // 取得先のURL
+    const url = 'http://localhost:8888'
+    // リクエスト（Get）
+    const response = await $axios.$get(url)
+    // 配列で返ってくるのでJSONにして返却
+    return {
+      name: response,
+    }
+  },
+  data() {
+    return {
+      name: null,
+    }
+  },
+}
+</script>
