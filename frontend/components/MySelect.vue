fecd7fec271abed88a973079b32ba35bcae04536
Yasunari t <u7df25c588m737@gmail.com>
2021-11-12 10:59:40 +0900

    Refactor: Prefector Select Tag

---
+++ frontend/components/MySelect.vue
@@ -0,0 +1,23 @@
+<template>
+  <select name="pref" class="bg-gray-100 rounded-sm">
+    <option
+      v-for="prefecture in prefectures"
+      :key="prefecture"
+      :value="prefecture"
+      :selected="prefecture === '東京都'"
+    >
+      {{ prefecture }}
+    </option>
+  </select>
+</template>
+
+<script>
+import { prefectures } from '@/utils/prefectures'
+export default {
+  data() {
+    return {
+      prefectures: prefectures,
+    }
+  },
+}
+</script>
