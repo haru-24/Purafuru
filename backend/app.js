dc0ac4ad74fd9308c67ecaf017122e925f2a4721
haru-24 <h24wler@gmail.com>
2021-11-14 17:57:16 +0900

    add:mysqlと接続

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8888;
-
+const mysql = require("mysql");
 const cors = require("cors");
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "nishiura",
+  database: "sample",
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!!!");
+  const sql = "select * from users";
+  con.query(sql, function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
 
 app.use(cors());
 
