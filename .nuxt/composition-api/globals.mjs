8752e62880d0be17eee9c084b10a621829f78779
haru-24 <h24wler@gmail.com>
2021-12-03 22:50:46 +0900

    delete:gitのキャッシュを削除

--- .nuxt/composition-api/globals.mjs
+++
@@ -1,5 +0,0 @@
-export const isFullStatic = false
-export const staticPath = "/Users/nishiuraharu/Desktop/share_tori_frontend/frontend/.nuxt/static-json"
-export const publicPath = "/"
-export const globalContext = "__NUXT__"
-export const globalNuxt = "$nuxt"
--- .nuxt/middleware.js
+++
@@ -1,9 +0,0 @@
-const middleware = {}
-
-middleware['authComplete'] = require('../middleware/authComplete.ts')
-middleware['authComplete'] = middleware['authComplete'].default || middleware['authComplete']
-
-middleware['authFilter'] = require('../middleware/authFilter.ts')
-middleware['authFilter'] = middleware['authFilter'].default || middleware['authFilter']
-
-export default middleware
